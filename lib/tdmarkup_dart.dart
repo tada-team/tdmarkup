f791dc2a8d044d7bd2c281ca3fd8cb9108b23d3f
eboshare <t.lila@list.ru>
2020-12-30 01:58:32 +0300

    style: improve exports in tdmarkup_dart.dart

--- lib/tdmarkup_dart.dart
+++ lib/tdmarkup_dart.dart
@@ -1,10 +1,14 @@
 library tdmarkup_dart;
 
+// Widgets.
 export './src/markup_text.dart';
+
+// View model.
 export './src/view_model/markup_view_model.dart';
+export './src/view_model/text_type.dart';
+
+// View model: Nodes.
 export './src/view_model/nodes/i_markup_node.dart';
-export './src/view_model/nodes/markup_node.dart';
 export './src/view_model/nodes/markup_node.dart';
 export './src/view_model/nodes/style_node.dart';
 export './src/view_model/nodes/text_node.dart';
-export './src/view_model/text_type.dart';
