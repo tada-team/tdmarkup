7970957ce8dac5ef18d373407e7b65cd2f094831
eboshare <t.lila@list.ru>
2020-12-30 01:41:41 +0300

    docs: add docs to i_markup_node.dart

--- lib/src/view_model/nodes/i_markup_node.dart
+++ lib/src/view_model/nodes/i_markup_node.dart
@@ -1,8 +1,15 @@
 import 'package:tdmarkup_dart/tdmarkup_dart.dart';
 
 abstract class IMarkupNode {
+  /// Text type.
   TextType get type;
+
+  /// Own text content.
   String get text;
+
+  /// Direct url for [TextType.link]
   String get url;
+
+  /// Nested [IMarkupNode]s.
   List<IMarkupNode> get children;
 }
